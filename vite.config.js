013e19e37c1dc1cfc5f8b1f8d86e2920a1a3259c
Sergiofggtff <2281830298@qq.com>
2026-08-02 22:24:39 +0800

    Disable minification for WebGPU build

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,8 @@ export default {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
         sourcemap: false, // Add sourcemap
-        target: 'esnext'
+        target: 'esnext',
+        minify: false
     },
     plugins:
     [
